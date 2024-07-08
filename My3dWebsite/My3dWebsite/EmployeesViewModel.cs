a1ac9371bfdfc109d2291a27e24e98d7b96eab11
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2024-07-08 20:49:18 +0200

    checkpoint1

--- My3dWebsite/My3dWebsite/EmployeesViewModel.cs
+++ My3dWebsite/My3dWebsite/EmployeesViewModel.cs
@@ -31,11 +31,11 @@ namespace My3dWebsite
         {
             var fakeEmployees = new[]
             {
-                new EmployeeViewModel { Id = 1, FirstName = "John", LastName = "Doe", Position = "Software Engineer", ProfileImage = "" },
-                new EmployeeViewModel { Id = 2, FirstName = "Jane", LastName = "Smith", Position = "Project Manager", ProfileImage = "" },
-                new EmployeeViewModel { Id = 3, FirstName = "Michael", LastName = "Brown", Position = "Designer", ProfileImage = "" },
-                new EmployeeViewModel { Id = 4, FirstName = "Emily", LastName = "Davis", Position = "QA Engineer", ProfileImage = "" },
-                new EmployeeViewModel { Id = 5, FirstName = "David", LastName = "Wilson", Position = "DevOps Engineer", ProfileImage = "" }
+                new EmployeeViewModel { Id = 1, FirstName = "Bill", LastName = "Doe", Position = "Software Engineer", ProfileImage = "Images/profile_man1.png" },
+                new EmployeeViewModel { Id = 2, FirstName = "Jane", LastName = "Smith", Position = "Project Manager", ProfileImage = "Images/profile_girl1.png"},
+                new EmployeeViewModel { Id = 3, FirstName = "Michael", LastName = "Brown", Position = "Designer", ProfileImage = "Images/profile_man2.png" },
+                new EmployeeViewModel { Id = 4, FirstName = "Emily", LastName = "Davis", Position = "QA Engineer", ProfileImage = "Images/profile_girl1.png" },
+                new EmployeeViewModel { Id = 5, FirstName = "John", LastName = "Wilson", Position = "DevOps Engineer", ProfileImage = "Images/profile_man1.png" }
             };
 
             foreach (var employee in fakeEmployees)
