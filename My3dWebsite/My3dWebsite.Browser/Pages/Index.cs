3d88eed41bb460ec7b3f06d12a8f10ecb20c8600
Andrei Arekhva <a.arekhva@gmail.com>
2025-04-08 11:09:20 +0300

    Upgrade to latest XR#

--- My3dWebsite/My3dWebsite.Browser/Pages/Index.cs
+++ My3dWebsite/My3dWebsite.Browser/Pages/Index.cs
@@ -1,9 +1,6 @@
-﻿using DotNetForHtml5;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
-using Microsoft.JSInterop;
-using My3dWebsite.Browser.Interop;
-using System;
+using OpenSilver.WebAssembly;
 using System.Threading.Tasks;
 
 namespace My3dWebsite.Browser.Pages
@@ -15,20 +12,15 @@ namespace My3dWebsite.Browser.Pages
         {
         }
 
-        protected async override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
 
-            if (!await JSRuntime.InvokeAsync<bool>("getOSFilesLoadedPromise"))
+            await Runner.RunApplicationAsync(async () =>
             {
-                throw new InvalidOperationException("Failed to initialize OpenSilver. Check your browser's console for error details.");
-            }
-
-            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
-            Program.RunApplication();
+                await XRSharp.Root3D.Initialize();
+                return new My3dWebsite.App();
+            });
         }
-
-        [Inject]
-        private IJSRuntime JSRuntime { get; set; }
     }
 }
--- My3dWebsite/My3dWebsite.Browser/Program.cs
+++ My3dWebsite/My3dWebsite.Browser/Program.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace My3dWebsite.Browser
 {
@@ -13,20 +9,8 @@ namespace My3dWebsite.Browser
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             var host = builder.Build();
             await host.RunAsync();
-        }
-
-        public static void RunApplication()
-        {
-            Application.RunApplication(async () =>
-            {
-                await XRSharp.Root3D.Initialize();
-                var app = new My3dWebsite.App();
-            });
         }
     }
 }
